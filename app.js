2050dc0f838c8ea1438296da4cff1fb4701e0411
Rishi Prasad Sharma <irishisharma026@gmail.com>
2023-02-12 23:36:05 +0530

    add database

--- app.js
+++ app.js
@@ -1,28 +1,141 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const lodash = require("lodash");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-let tasks = [];
-let workItems = [];
+// connecting mongoose database
+mongoose.set("strictQuery", false);
+mongoose.connect(
+  "mongodb+srv://admin-rishi:rishi-admin@atlascluster.e15qmbl.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true },
+  () => {
+    console.log("Database connected successfully");
+  }
+);
+// getting updated date
+const day = date.getDate();
 
-app.get("/", (req, res) => {
-  let day = date.getDate();
-  res.render("list", { kindOfDay: day, newItems: tasks });
+// creating schema for todolist items
+const itemSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
 });
 
+const listSchema = mongoose.Schema({
+  name: String,
+  listItems: [itemSchema],
+});
+
+const Task = mongoose.model("Task", itemSchema);
+const List = mongoose.model("List", listSchema);
+
+// preloaded documents by default
+const t1 = new Task({
+  name: "Morning Pooja",
+});
+
+const t2 = new Task({
+  name: "Study",
+});
+
+const t3 = new Task({
+  name: "Play Game",
+});
+
+const preLoadItems = [t1, t2, t3];
+
+app.get("/", (req, res) => {
+  // getting data back from the database
+  Task.find((err, task) => {
+    if (task.length === 0) {
+      Task.insertMany(preLoadItems, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Data inserted");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { kindOfDay: day, newItems: task });
+    }
+  });
+});
+
+// get request for creating dynamic pages
+app.get("/:listId", (req, res) => {
+  const listID = lodash.capitalize(req.params.listId);
+
+  List.findOne({ name: listID }, (err, foundList) => {
+    if (!err) {
+      if (!foundList) {
+        // creating a new list
+        const list = new List({
+          name: listID,
+          listItems: preLoadItems,
+        });
+        list.save();
+        res.redirect("/" + listID);
+      } else {
+        res.render("list", {
+          kindOfDay: foundList.name,
+          newItems: foundList.listItems,
+        });
+      }
+    }
+  });
+});
+
+// post request for addding tasks in the list
 app.post("/", (req, res) => {
-  let task = req.body.task;
-  console.log(req.body);
-  if (req.body.list === "Work List") {
-    workItems.push(task);
-    res.redirect("/work");
-  } else {
-    tasks.push(task);
+  const taskName = req.body.task;
+  const listName = req.body.list;
+  // inserting new items by user
+  const task = new Task({
+    name: taskName,
+  });
+  if (listName === day) {
+    // saving the inserted item to data base
+    task.save();
+    // redirecting back to the gome route
     res.redirect("/");
+  } else {
+    List.findOne({ name: listName }, (err, foundList) => {
+      foundList.listItems.push(task);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
+});
+
+//post request for deleting item from the todo list
+app.post("/delete", (req, res) => {
+  const checkedItem = req.body.checkedItem;
+  const listName = req.body.listName;
+
+  if (listName === day) {
+    const del = new Task({
+      _id: checkedItem,
+    });
+    del.remove();
+    res.redirect("/");
+  } else {
+    List.findOneAndUpdate(
+      { name: listName },
+      { $pull: { listItems: { _id: checkedItem } } },
+      (err, foundList) => {
+        if (!err) {
+          res.redirect("/" + listName);
+        }
+      }
+    );
   }
 });
 
